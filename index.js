53cf641f0793d10c476e1d292f101a23847170a6
thalesAlves758 <thalesalves758@gmail.com>
2022-06-06 10:55:52 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const a = 1;
+const b = 2;
+const c = 'teste';
+
+console.log(a == b);
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tabWith: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+};
